929e5dd5a30a12827af5b71b3a41f63b4404da91
Yakov <kudrya_yakov@mail.com>
2022-10-12 21:54:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter import ttk
+from tkinter import messagebox as mb
+from PIL import Image
+
+
+def choosefile():
+    pass
+
+
+def choosefile_test():
+    pass
